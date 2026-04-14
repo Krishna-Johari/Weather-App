3174ece4b4632cdf9957a445c5acf05d27310e56
Krishna Johari <joharikrishna13@gmail.com>
2026-04-14 12:03:10 +0530

    fix: update CORS for production

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.get('/api/weather', async (req, res) => {
